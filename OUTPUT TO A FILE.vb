857ac4709bcaf3e8f0e00761ffd06af7ad23f1cb
Zain Momin <55015838+MZM45@users.noreply.github.com>
2020-01-25 23:51:52 +0500

    Add files via upload

---
+++ OUTPUT TO A FILE.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim GRNO As Integer
+        Dim sName, sClass As String
+        Dim sFee As Decimal
+        Dim isFeePaid As Boolean
+        Dim more As Char
+
+        GRNO = 0
+        sName = ""
+        sClass = ""
+        sFee = 0
+        isFeePaid = False
+        more = "N"
+
+        FileOpen(1, "C:\filePractice\ABC.txt", OpenMode.Append)
+        Do
+            Console.Write("Enter GR No: ") : GRNO = Console.ReadLine
+            Console.Write("Enter student name: ") : sName = Console.ReadLine
+            Console.Write("Enter student class: ") : sClass = Console.ReadLine
+            Console.Write("Enter fee amount: ") : sFee = Console.ReadLine
+            Console.Write("Is fee paid? ") : isFeePaid = Console.ReadLine
+
+            WriteLine(1, GRNO)
+            WriteLine(1, sName)
+            WriteLine(1, sClass)
+            WriteLine(1, sFee)
+            WriteLine(1, isFeePaid)
+
+            Console.Write("Do you want to enter another record? (Y/N) ")
+            more = Console.ReadLine
+        Loop Until UCase(more) = "N"
+        FileClose(1)
+    End Sub
+
+End Module
---
+++ Searching Record.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim GRNO, GRVal As Integer
+        Dim sName, sClass As String
+        Dim sFee As Decimal
+        Dim isFeePaid, isFound As Boolean
+
+        GRNO = 0
+        GRVal = 0
+        sName = ""
+        sClass = ""
+        sFee = 0
+        isFeePaid = False
+        isFound = False
+
+        Console.Write("Enter GR number to search for: ") : GRVal = Console.ReadLine()
+
+        FileOpen(1, "C:\filePractice\ABC.txt", OpenMode.Input)
+        While Not EOF(1)
+            GRNO = LineInput(1)
+            Input(1, sName)
+            Input(1, sClass)
+            Input(1, sFee)
+            Input(1, isFeePaid)
+
+            If GRNO = GRVal Then
+                isFound = True
+                Console.WriteLine("Enter GR No: " & GRNO)
+                Console.WriteLine("Enter student name: " & sName)
+                Console.WriteLine("Enter student class: " & sClass)
+                Console.WriteLine("Enter fee amount: " & sFee)
+                Console.WriteLine("Is fee paid? " & isFeePaid)
+                Console.WriteLine()
+            End If
+        End While
+        FileClose(1)
+
+        If Not isFound Then
+            Console.WriteLine("Record for the GR number " & GRVal & " couldn't be found...")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
